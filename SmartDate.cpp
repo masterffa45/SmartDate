75571d1fb531bcbd991ddc6846e403b8e4caca6f
David <DA@VID.cum>
2023-04-25 21:11:28 +0300

    The class moved to header file

--- SmartDate.cpp
+++ SmartDate.cpp
@@ -2,24 +2,8 @@
 * Проект "Класс Умная дата"
 */
 #include <iostream>
+#include "SmartDate.h"
 using namespace std;
-class SmartDate {
-private:
-	short day;
-	short month;
-	short year;
-public:
-	SmartDate(short _d, short _m, short _y);
-};
-SmartDate::SmartDate(short _d, short _m, short _y) {
-	day = _d;
-	month = _m;
-	year = _y;
-
-}
-
-
-
 int main()
 {
 	SmartDate d1(25, 04, 2023);
