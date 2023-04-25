975cdd5f1b587589715c2f8d84d30233ad1b1e86
David <DA@VID.cum>
2023-04-25 21:00:08 +0300

    Defined class SmartDate

--- SmartDate.cpp
+++ SmartDate.cpp
@@ -3,12 +3,26 @@
 */
 #include <iostream>
 using namespace std;
+class SmartDate {
+private:
+	short day;
+	short month;
+	short year;
+public:
+	SmartDate(short _d, short _m, short _y);
+};
+SmartDate::SmartDate(short _d, short _m, short _y) {
+	day = _d;
+	month = _m;
+	year = _y;
+
+}
+
+
+
 int main()
 {
-	class Date {
+	SmartDate d1(25, 04, 2023);
 
-
-
-   };
 }
 
